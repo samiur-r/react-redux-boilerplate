cf9360b90a44e461680ef226144d985353b2334e
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-06-29 19:06:08 +0600

    feat(theme): add theme config

---
+++ src/config/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        light:"#27293D",
+        main: "#1E88F7",
+        dark: "#1E1E2E",
+    },
+     type: 'light',
+  },
+});
+
+export default theme
