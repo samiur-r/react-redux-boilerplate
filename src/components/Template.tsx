48271a54bf4ecce1200688b5b48222cd8763adab
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-06-29 23:20:08 +0600

    feat(component): add a template for components

---
+++ src/components/Template.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+interface TemplateProps {
+  propsOne: number
+  propsTwo: (someVar: Array<string>) => void
+}
+
+const Template: React.FC<TemplateProps> = ({ propsOne, propsTwo }) => {
+    const [count, setCount] = useState<number>(propsOne);
+    const handleCount = () => {
+        setCount((curr) => curr + 1)
+		propsTwo(["apples", "oranges"])
+    }
+    return (
+        <div>
+            <button onClick={handleCount}>increment</button>
+            <div>{count}</div>
+        </div>
+    )
+}
+
+export default Template;
