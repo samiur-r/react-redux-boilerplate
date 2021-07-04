09b2ca5eb3a2b6537a1479fde0c6f12641b7cfff
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-07-05 03:14:47 +0600

    refactor(todo-api): add types

--- src/store/api/todosApi.ts
+++ src/store/api/todosApi.ts
@@ -1,6 +1,6 @@
 import api from './api'
 
-interface TodoType {
+interface TodosType {
 	id: number,
 	todo: string,
 	completed: boolean
@@ -8,32 +8,32 @@ interface TodoType {
 
 export default class TodosApi {
 
-	static getTodos() {
-	  return new Promise((resolve, reject) => {
+	static getTodos(): Promise<TodosType> {
+	  return new Promise<TodosType>((resolve, reject) => {
 		api.get('/todos')
 		.then( res => resolve(res.data) )
 		.catch( err => reject(err.message) )
 	  });
 	}
 
-	static addTodo(todo: TodoType) {
-		return new Promise((resolve, reject) => {
+	static addTodo(todo: TodosType): Promise<TodosType> {
+		return new Promise<TodosType>((resolve, reject) => {
 		  api.post('/todos', todo)
 		  .then( res => resolve(res.data) )
 		  .catch( err => reject(err.message) )
 		});
 	}
 
-	static updateTodo(id:number, todo: TodoType) {
-		return new Promise((resolve, reject) => {
+	static updateTodo(id:number, todo: TodosType): Promise<TodosType> {
+		return new Promise<TodosType>((resolve, reject) => {
 		  api.put('/todos/'+id, todo)
 		  .then( res => resolve(res.data) )
 		  .catch( err => reject(err.message) )
 		});
 	}
 
-	static deleteTodo(id:number) {
-		return new Promise((resolve, reject) => {
+	static deleteTodo(id:number): Promise<TodosType> {
+		return new Promise<TodosType>((resolve, reject) => {
 		  api.delete('/todos/'+id)
 		  .then( res => resolve(res.data) )
 		  .catch( err => reject(err.message) )
--- src/store/reducers/todosSlice.ts
+++ src/store/reducers/todosSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface TodosType {
+  id: number,
   todo: string,
   completed: boolean
 }
