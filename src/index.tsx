90f8bca70b4d33a2138da70545f5162686194783
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-07-04 18:14:02 +0600

    chore(store): configure redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { ThemeProvider } from '@material-ui/core';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import theme from './config/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+	<Provider store={store}>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
